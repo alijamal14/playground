b9b96861afe041958edf90b3308d8c696b9dbd08
Ali Jamal <alijamal14@live.com>
2021-12-08 17:11:29 +0500

    Added Two more cases - Only Numbers with more than 6 charcters, hypen will be appended after left most two - if left characters are alpha next two digits will be appended before hypen

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 function convertToOldMRN(NewMRN) {
     if (NewMRN) {
         NewMRN = NewMRN.toUpperCase();
-        let splittedMRN = NewMRN.split('-');
+        let splittedMRN = NewMRN.split('-');        
         if (splittedMRN?.[0].length === 2 && splittedMRN?.[0].match(/^[a-z]+$/i)) {//This means its new MR Number
             let left = splittedMRN[0] += splittedMRN[1].substring(0, 2);
             let right = splittedMRN[1].substring(2);
             return left + '-' + right;
+        } else if (NewMRN.length > 6 && /^\d+$/.test(NewMRN)) {/// Only Numbers with more than 6 charcters, hypen will be appended after left most two
+            NewMRN = NewMRN.substring(0, 2) + '-' + NewMRN.substring(2);
+        } 
+        else if (NewMRN.length > 4 && /^[a-z]{2}/i.test(NewMRN)) { /// if left characters are alpha next two digits will be appended before hypen
+            NewMRN = NewMRN.substring(0, 4) + '-' + NewMRN.substring(4);
         }
     }
     return NewMRN;
